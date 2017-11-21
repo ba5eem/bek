c7943aa6c34024bca82194770448f9ced663bade
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-11-20 16:54:46 -1000

    react native navigator installed and set up

--- android/app/src/main/java/com/bek/MainApplication.java
+++ android/app/src/main/java/com/bek/MainApplication.java
@@ -1,8 +1,3 @@
-package com.bek;
-
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,7 +7,7 @@ import com.reactnativenavigation.NavigationApplication;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -43,11 +38,6 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
-}
-
-
-
-public class MainApplication extends NavigationApplication {
 
   @Override
   public boolean isDebug() {
@@ -55,16 +45,10 @@ public class MainApplication extends NavigationApplication {
     return BuildConfig.DEBUG;
   }
 
-  protected List<ReactPackage> getPackages() {
-    // Add additional packages you require here
-    // No need to add RnnPackage and MainReactPackage
-    return Arrays.<ReactPackage>asList(
-      // eg. new VectorIconsPackage()
-    );
-  }
-
+  @Nullable
   @Override
   public List<ReactPackage> createAdditionalReactPackages() {
     return getPackages();
   }
+
 }
