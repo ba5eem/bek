dc40b2258f7667af1428e21bbd91cdcfafa6b84f
Ba5eem <baseem@gmail.com>
2017-11-21 06:34:12 -1000

    react-native file structure

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,59 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { loginUser } from '../../actions/login';
-import { filterAll } from '../../lib/Filters';
-// import {
-//   Button,
-//   ScrollView,
-//   StyleSheet,
-//   Text,
-//   TextInput,
-//   TouchableHighlight,
-//   View
-// } from 'react-native';
+import Login from './Login'
+import {connect} from 'react-redux'
+import * as actions from './actions' // mapDispatchToProps
+//import {getNav, getLogin} from '../../reducers/rootReducer'
 
-//react-native import has been commented out - please un-comment once react-native has been installed. Link and Connect may not be neccessary moving forward with react-native
+const mapStateToProps = (state) => ({
+  ...getNav(state),
+  ...getLogin(state),
+})
 
-//Using react-native - these boiler plates may be un-neccessary - please remove/change/add as you see fit: 
-
-class Login extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      shifts: ''
-    }
-  }
-
-
-  handleSubmit(){
-    //this.props.loginUser();
-  }
-
-
-  render(){
-
-    return(
-       <div>Login Container</div>
-    )
-  }
-}
-
-
-
-//The methods built in are boiler plater setup - they may not be neccessary or applicable to this containers - please remove/add as needed. - they are only here to help with startup of project
-
-
-const mapStateToProps = (state) => {
-  return{
-    shifts: state.shifts
-  }
-}
-
-const ConnectedLogin = connect(
-  mapStateToProps,
-  {loginUser}
-)(Login)
-
-export default ConnectedLogin;
+export default connect(mapStateToProps, actions)(Login)
