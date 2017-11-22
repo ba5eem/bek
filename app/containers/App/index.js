91baf597c7801b6889bb96b4ea9451fa08df18be
Ella Mae A <ellamae207@gmail.com>
2017-11-21 15:33:28 -1000

    chat now works, signing in with alias

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -17,7 +17,10 @@ import { Provider } from 'react-redux';
 import reducers from '../../reducers';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import Login from '../Login';
+import Chat from '../Chat';
+import Home from '../HomeTest';
+import { Router, Scene } from 'react-native-router-flux';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -46,18 +49,26 @@ export default class app extends Component<{}> {
   render() {
     return (
       <Provider store={store}>
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to BEK!
-        </Text>
-        {/*<Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>*/}
-        <Login/>
-      </View>
+        <Router>
+          {/*<View style={styles.container}>
+            <Text style={styles.welcome}>
+              Welcome to BEK!
+            </Text>
+            {/*<Text style={styles.instructions}>
+              To get started, edit App.js
+            </Text>
+            <Text style={styles.instructions}>
+              {instructions}
+            </Text>*
+
+
+                <Chat/>
+          </View>*/}
+          <Scene key='root' style={{paddingTop: Platform.OS === 'ios' ? 64 : 54}}>
+          <Scene key='home' title='Home' component={Home}/>
+          <Scene key='chat' title='Chat' component={Chat}/>
+        </Scene>
+        </Router>
       </Provider>
     );
   }
