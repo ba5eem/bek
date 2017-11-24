fbb2f103ed05dfbf8ab96358f4b5fec136d3629a
Ella Mae A <ellamae207@gmail.com>
2017-11-24 11:42:10 -1000

    setup twilio and can send sms

---
+++ server/twilio.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+console.log(process.env.TWILIO_ACCOUNT_SID);
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+client.messages.create({
+  from: process.env.TWILIO_PHONE_NUMBER,
+  to: '+18087280608',
+  body: "Hi BEK Team! Twilio was succesfully set up"
+}).then((messsage) => console.log(message.sid));
