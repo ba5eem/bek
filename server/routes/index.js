2918eb98837c12dfa728a189ed1f707221ee55f0
Ella Mae A <ellamae207@gmail.com>
2017-11-24 14:56:45 -1000

    install chance npm

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,13 @@ const route       = express.Router();
 const users       = require('./users.js');
 const auth        = require('./auth.js');
 const shifts      = require('./calendar.js');
-
+const chat        = require('./twilioChat.js');
 
 
 route.use('/shifts', shifts);
 route.use('/auth', auth);
 route.use('/users', users);
+route.use('/chat', chat);
 
 
 module.exports = route;
