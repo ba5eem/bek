0d83649184e86598ff770eefed7a5082303bb2bc
Ella Mae A <ellamae207@gmail.com>
2017-11-27 10:35:21 -1000

    user components, details & lists

---
+++ src/components/UserList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserDetailView from './UserDetailView';
+
+
+const UserList = ({users}) => {
+  return (
+    <div className='all-user-list'>
+     {
+        users.map((user,idx) => {
+          return (
+            <UserDetailView
+              key={idx}
+              id={user.id}
+              name={user.name}
+              givenName={user.givenName}
+              familyName={user.familyName}
+              email={user.email}
+              image={user.imageUrl}
+            />
+          )
+        })
+      }
+    </div>
+
+  )
+}
+
+
+export default UserList;
+
