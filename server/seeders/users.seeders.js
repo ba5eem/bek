5351309f8d66e2062856470972531460a2f6b3fa
Ba5eem <baseem@gmail.com>
2017-11-28 08:03:43 -1000

    added admin to user model

--- server/seeders/users.seeders.js
+++ server/seeders/users.seeders.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
     {
-      email: 'helloworld@gmail.com',
+      email: 'helloworld@gmailbek.com',
       familyname: 'Smith',
       givenname: 'John',
       googleid: '112587183780205170008',
@@ -16,7 +16,19 @@ module.exports = {
       updatedAt: '2017-11-23 01:00:57'
     },
     {
-      email: 'coder4life@gmail.com',
+      email: 'cohortuser19@gmail.com',
+      familyname: 'user',
+      givenname: 'cohort',
+      googleid: '112587183780205170008',
+      image: 'https://png.icons8.com/nolan/1600/admin',
+      phone: '6129982261',
+      name: 'cohort user',
+      admin: true,
+      createdAt: '2017-11-23 00:00:57',
+      updatedAt: '2017-11-23 01:00:57'
+    },
+    {
+      email: 'coder4life@gmailbek.com',
       familyname: 'Hunt',
       givenname: 'Ethan',
       googleid: '113587183780215170004',
@@ -28,19 +40,19 @@ module.exports = {
       updatedAt: '2017-11-23 01:00:57'
     },
     {
-      email: 'abc123@gmail.com',
+      email: 'abc123@gmailbek.com',
       familyname: 'Elememtary',
       givenname: 'Sherlock',
       googleid: '112586183780205170509',
       phone: '8087280608',
       image: 'http://www.eds.org.nz/assets/img/head%20and%20shoulders%20image%20male.png',
       name: 'Sherlock_Elementary',
-      admin: true,
+      admin: false,
       createdAt: '2017-11-23 00:00:57',
       updatedAt: '2017-11-23 01:00:57'
     },
     {
-      email: 'bestworker@gmail.com',
+      email: 'bestworker@gmailbek.com',
       familyname: 'Ligent',
       givenname: 'Dee',
       googleid: '113587183780205171107',
@@ -52,14 +64,14 @@ module.exports = {
       updatedAt: '2017-11-23 01:00:57'
     },
     {
-      email: 'controllfreak@gmail.com',
+      email: 'controllfreak@gmailbek.com',
       familyname: 'Lock',
       givenname: 'Amy',
       googleid: '112587183780205100102',
       image: 'http://www.eds.org.nz/assets/img/head%20and%20shoulders%20image%20male.png',
       phone: '6129982261',
       name: 'Amy_Lock',
-      admin: true,
+      admin: false,
       createdAt: '2017-11-23 00:00:57',
       updatedAt: '2017-11-23 01:00:57'
     }], {});
