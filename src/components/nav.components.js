942b7258f4044a9439b062e784b770bcdcc0f0a5
Ella Mae A <ellamae207@gmail.com>
2017-11-27 19:24:29 -1000

    nav delete

--- src/components/nav.components.js
+++ src/components/nav.components.js
@@ -2,31 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-  if(localStorage.admin){
     return (
       <div className="nav-links">
-        <Link to="/">Home</Link>
-        |
-        <Link to="/">Management</Link>
-        |
-        <Link to="/">Front</Link>
-        |
-        <Link to="/">Back</Link>
-        |
-        <Link to="/logout">Logout</Link>
-      </div>
-    )
-  }else{
-    return (
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        |
-        <Link to="/profile">My Profile</Link>
-        |
-        <Link to="/login">Login</Link>
       </div>
     );
-  }
+
 }
 
 export default Nav;
