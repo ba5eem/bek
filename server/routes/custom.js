397bb4c6e4232e5aff43fb1becb3fad6b318f308
Ba5eem <baseem@gmail.com>
2017-11-29 11:36:58 -1000

    custom shift works

--- server/routes/custom.js
+++ server/routes/custom.js
@@ -17,66 +17,54 @@ let jwtClient     = new google.auth.JWT(
 
 
 route.post('/', (req,res) => {
-//authenticate request
-  //console.log(req.body);
-  let time = req.body._4
-  let today = moment().format('YYYY-MM-DDT');
-  let start = today+time+':00-'+time;
-  let endTime = parseInt(time)+4+':00';
-  let end = today+endTime+':00-'+endTime;
-  console.log(time);
-  console.log(today);
-  console.log(start)
-  console.log(endTime);
-  console.log(end)
+  let time = req.body
+  let date = req.body.month+req.body.day+"T";
+  let start = date+req.body.start+':00';
+  let end = date+req.body.end+':00';
 
 
-var event = {
-  'title':req.body.title,
-  'summary': req.body.summary,
-  'location': "Manoa Innovation Center",
-  'description': req.body.description,
-  'start': {
-    'dateTime': start,
-    'timeZone': 'America/Adak',
-  },
-  'end': {
-    'dateTime': end,
-    'timeZone': 'America/Adak',
-  },
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-};
-jwtClient.authorize(function (err, tokens) {
-  
- if (err) {
-   console.log("Did not connect!", err);
-   return;
- } else {
-   console.log("Successfully connected!");
-   
+  var event = {
+    'title':req.body.title,
+    'summary': req.body.summary,
+    'location': "Manoa Innovation Center",
+    'description': req.body.description,
+    'start': {
+      'dateTime': start + '-'+req.body.start+':00',
+      'timeZone': 'America/Adak',
+    },
+    'end': {
+      'dateTime': end + '-'+req.body.end+':00',
+      'timeZone': 'America/Adak',
+    },
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  };
+  jwtClient.authorize(function (err, tokens) {
+    if (err) { console.log("Did not connect!", err);
+     return; } 
+    else { console.log("Successfully connected!");
 
-let token = tokens.access_token;
-let calendarId = 'cohortuser19@gmail.com';
-let calendar = google.calendar('v3');
-gcal(token).events.insert(calendarId,event, function(err,data){
-  if(err) console.log(500,err);
-  return calendar.events.list({
-    auth: jwtClient,
-    calendarId: 'cohortuser19@gmail.com'
-  }, function(err,response){
-      var events = response.items;
-
-      res.json(events);
-  })
-  })
- }
-});
+      let token = tokens.access_token;
+      let calendarId = 'cohortuser19@gmail.com';
+      let calendar = google.calendar('v3');
+        gcal(token).events.insert(calendarId,event, function(err,data){
+          if(err) console.log(500,err);
+          
+            return calendar.events.list({
+              auth: jwtClient,
+              calendarId: 'cohortuser19@gmail.com'
+              }, function(err,response){
+                  var events = response.items;
+                  res.json(events);
+          })
+        })
+     }
+  });
 })
 
 
