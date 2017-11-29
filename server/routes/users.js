db4a1edb1ea2695d0ec254402eaa8288c5b38aff
Ba5eem <baseem@gmail.com>
2017-11-28 16:03:51 -1000

    fix er

--- server/routes/users.js
+++ server/routes/users.js
@@ -36,34 +36,8 @@ route.get('/:id', ( req, res ) => {
   });
 });
 
-<<<<<<< HEAD
-route.post('/', (req,res) => {
-  let body = req.body;
-  let local = {};
-  console.log('posting body--->', body)
-  user.create({
-    email: body.email,
-    familyname: body.familyName,
-    givenname: body.givenName,
-    googleid: body.googleId,
-    image: body.imageUrl,
-    name: body.name,
-    admin: false
-  })
-  .then((user) => {
-    console.log('user ---->serverroutes,', user)
-    local.fullname = user.name;
-    local.id = user.id;
-    local.image = user.image;
-    res.json(local)
-  })
-  .catch((err) => {
-    return res.json(null)
-  })
-})
-=======
 
->>>>>>> development
+
 
 route.put('/:id/editphone', ( req, res ) => {
   let id = req.params.id;
