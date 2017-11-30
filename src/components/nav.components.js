9a1bdc90f97ab3992ccbb041dec725a76634f1f5
Ba5eem <baseem@gmail.com>
2017-11-29 14:34:27 -1000

    user name and group to headline

--- src/components/nav.components.js
+++ src/components/nav.components.js
@@ -2,8 +2,13 @@ import React from 'react';
 //import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    const user = localStorage.username;
+    const group = localStorage.group;
     return (
       <div className="nav-links">
+        <div className="welcomeUserSign">
+          Welcome {user}, your group name is {group}
+        </div>
       </div>
     );
 
