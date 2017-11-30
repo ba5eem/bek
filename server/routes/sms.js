2a6b20947efdea6038cf6fa13350a3be7f80d4ef
Ella Mae A <ellamae207@gmail.com>
2017-11-29 17:33:49 -1000

    sms response

--- server/routes/sms.js
+++ server/routes/sms.js
@@ -4,7 +4,7 @@ const app         = express();
 const route       = express.Router();
 const db          = require('../models');
 const {user}      = db;
-
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 route.post('/', (req,res) => {
   console.log('sms from routes--->',req.body)
@@ -56,23 +56,40 @@ route.post('/absent', (req,res) => {
 
 
   numbers.forEach(function(phones){
-  let content = {
-      phone: phones,
-      payload: `Someone Called Out - Shift just became available! Shift Details here: ${link}`
-    }
-    const client = require('twilio')(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
-  client.messages.create({
-    from: process.env.TWILIO_PHONE_NUMBER,
-    to: "+1"+ content.phone,
-    body: content.payload
-  }).then(()=>{
-    console.log("succes");
-  })
+    let content = {
+        phone: phones,
+        payload: `Someone Called Out - Shift just became available! Shift Details here: ${link}`
+      }
+      const client = require('twilio')(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    );
+    client.messages.create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: "+1"+ content.phone,
+      body: content.payload
+    }).then(()=>{
+      console.log("succes");
+    })
   })
   res.json('succes');
+})
+
+
+
+route.post('/response', (req,res) => {
+  const twiml = new MessagingResponse();
+  console.log(req.body.Body);
+  console.log(req.body.From);
+  if(req.body.Body == 'yes'){
+    console.log('received yes reply')
+    twiml.message('You replied YES, period')
+  }else {
+    console.log('received yes reply')
+    twiml.message('You replied something else other than YES, bye')
+  }
+  console.log('INCOMING MSG:', req.body.Body)
+  res.json(data)
 })
 
 module.exports = route;
