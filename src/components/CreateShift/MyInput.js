0a9fbba700f86305a24b3233d1710708fb4434a4
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-11-29 15:20:58 -1000

    create new shift restyled

--- src/components/CreateShift/MyInput.js
+++ src/components/CreateShift/MyInput.js
@@ -1,13 +1,13 @@
 // MyInput.js
 import { withFormsy } from 'formsy-react';
 import React from 'react';
- 
+
 class MyInput extends React.Component {
   constructor(props) {
     super(props);
     this.changeValue = this.changeValue.bind(this);
   }
- 
+
   changeValue(event) {
     // setValue() will set the value of the component, which in
     // turn will validate it and the rest of the form
@@ -15,11 +15,11 @@ class MyInput extends React.Component {
     // for Formsy to work.
     this.props.setValue(event.currentTarget.value);
   }
- 
+
   render() {
     // An error message is returned only if the component is invalid
     const errorMessage = this.props.getErrorMessage();
- 
+
     return (
       <div>
         <input
@@ -38,16 +38,10 @@ const input = {
   width: "400px",
   borderRadius: "5px",
   border: "2px solid #66b3ff",
-  fontSize:"40px",
+  fontSize:"30px",
   textAlign:"center",
   marginTop: "10px"
 
 }
-// const select = {
-//   width: "200px",
-//   textAlign:"center",
-//   backgroundColor: "#66b3ff",
-//   fontSize:"40px"
-// }
- 
+
 export default withFormsy(MyInput);
