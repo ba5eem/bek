c4943cf0297bb2b6ed00357261f25f554dc9f7a6
Ella Mae A <ellamae207@gmail.com>
2017-11-30 10:01:38 -1000

    added index route

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get('/', ( req, res ) =>{
 });
 
 app.get('*', ( req, res ) => {
-  res.sendFile(path.join(__dirname, '..', 'public'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 const server = app.listen(PORT,() => {
