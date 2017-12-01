e5062cb406360a30f6637bcb09e0ff343714a02f
Ba5eem <baseem@gmail.com>
2017-11-30 16:13:04 -1000

    assign shift

--- src/reducers/shifts.js
+++ src/reducers/shifts.js
@@ -1,4 +1,4 @@
-import { LOAD_SHIFTS,ADD_SHIFT,LOAD_SHIFT } from '../actions/shifts.js';
+import { LOAD_SHIFTS,ADD_SHIFT,LOAD_SHIFT,EDIT_SHIFT } from '../actions/shifts.js';
 import { SMS_SHIFT } from '../actions/sms';
 //var moment = require('moment');
 
@@ -42,8 +42,10 @@ const shifts = (state = [], action) => {
      return [ ...(state.slice(0, index)), res, ...(state.slice((index + 1), state.length))];
     case LOAD_SHIFT:
 
- 
       return [...action.shift];
+    case EDIT_SHIFT:
+
+     return state;
 
     default:
       return state
